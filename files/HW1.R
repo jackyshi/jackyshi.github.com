cce5fad2151fed3c217ec42dbeebd42b83e36dad
jackyshi <jackshi008@gmail.com>
2016-07-06 23:30:31 +0800

    mfin7023

--- files/HW1.R
+++ files/HW1.R
@@ -1,19 +1,19 @@
-T<-10
-n<-100
+T<-1
+n<-1000
 dt<-T/n
-d<-1000
+d<-500
 LHS<-rep(0,d)
 RHS<-rep(0,d)
 
 for(j in 1:d) {
-  W<-rnorm(n)
+  W<-rnorm(n+1)
   LHS[j]<-0
   for(i in 1:n) {
     Wt<-sum(W[1:i]*sqrt(dt))
-    LHS[j]<-LHS[j]+(W[i]*sqrt(dt)*Wt)
+    LHS[j]<-LHS[j]+(W[i+1]*sqrt(dt)*Wt)
   }
   
-  RHS[j]<-((sum(W[1:n]*sqrt(dt)))^2-T)/2+10
+  RHS[j]<-((sum(W[1:n]*sqrt(dt)))^2-T)/2
 }
 
 plot(LHS,RHS)
